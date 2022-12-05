03c4880ade291bf9ad1e13b898519cc02e65ac59
mishikasethi <116032782+mishikasethi@users.noreply.github.com>
2022-12-05 23:22:37 +0530

    Update Student.java

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -1,11 +1,9 @@
 package school.management.system;
 
 /**
- * Created by Rakshith on 4/3/2017.
  * This class is responsible for keeping the
  * track of students fees, name ,grade & fees
  * paid.
- *
  */
 public class Student {
 
